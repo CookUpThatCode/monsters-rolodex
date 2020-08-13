55a7acd1d0c2fe39f7c6bb5dee04bed6d05f0057
Matt English <matt.ee.english@gmail.com>
2020-08-13 15:58:08 -0700

    Updates

---
+++ precache-manifest.824fc05aaf17ca3ff4de23abdbd8247b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6c9685357dde9d8a4d39527c3749c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84e549fbfcafe912789",
+    "url": "/static/css/main.8f210293.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84e549fbfcafe912789",
+    "url": "/static/js/main.47833c50.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
